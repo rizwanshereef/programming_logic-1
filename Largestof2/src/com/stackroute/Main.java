c3f44c61dbff760d2bb41ad760a291395569f60d
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 21:23:14 +0530

    Updated comments

--- Largestof2/src/com/stackroute/Main.java
+++ Largestof2/src/com/stackroute/Main.java
@@ -1,22 +1,22 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/* Program to find the highest number*/
 public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+/*Input the numbers */
         System.out.println("Enter the 1st number:");
         int a = s.nextInt();
         System.out.println("Enter the 2nd number:");
         int b = s.nextInt();
-        if(a>b)
-        {
+/* Check the largest by comparing and display result*/
+        if(a>b){
             System.out.println("The Highest number is:"+a);
 
         }
-        else
-        {
+        else{
             System.out.println("The Highest number is:"+b);
         }
     }
