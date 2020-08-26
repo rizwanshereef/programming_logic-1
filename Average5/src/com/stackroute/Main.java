22ac8193104a49d38307a259c77c38f559259f86
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 21:08:51 +0530

    Updated comments.

--- Average5/src/com/stackroute/Main.java
+++ Average5/src/com/stackroute/Main.java
@@ -8,11 +8,13 @@ public class Main {
         Scanner s = new Scanner(System.in);
         float sum =0;
         int inp[]= new int[5];
+        /* Enter the number on to the array. */
         for (int i=0;i<5;i++) {
             System.out.println("Enter the " + (i + 1) + " number");
             inp[i] = s.nextInt();
             sum = sum + inp[i];
         }
+        /* find the average*/
         float avg = (sum/5);
         System.out.println("The Sum of 5 numbers is: "+sum);
         System.out.println("The Average of 5 numbers is: "+avg);
