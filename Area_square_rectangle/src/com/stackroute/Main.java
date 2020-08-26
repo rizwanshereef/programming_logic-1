12e9a17a7c5b0b321f7fe26ea83ff9318574b252
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 21:06:43 +0530

    Update Main.java

--- Area_square_rectangle/src/com/stackroute/Main.java
+++ Area_square_rectangle/src/com/stackroute/Main.java
@@ -3,20 +3,18 @@ package com.stackroute;
 import java.util.Scanner;
 
 public class Main {
-
+// main class to input lenght and breadth and calculate area of Square and Rectangle separately.
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the length of the Rectangle/Square:");
         int a = s.nextInt();
         System.out.println("Enter the breadth of the Rectangle/Square:");
         int b = s.nextInt();
-
-        if(a==b)
-        {
+// Check if it is Sqaure or Rectangle
+        if(a==b){
             System.out.println("The area of the Square is: "+(a*a));
         }
-        else
-        {
+        else{
             System.out.println("The area of the Rectangle is: "+(a*b));
         }
     }
