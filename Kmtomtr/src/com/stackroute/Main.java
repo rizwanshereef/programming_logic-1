8d362c77ce150113a6bf7f06369c8ef9269c4038
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 21:20:30 +0530

    Updated comments

--- Kmtomtr/src/com/stackroute/Main.java
+++ Kmtomtr/src/com/stackroute/Main.java
@@ -1,14 +1,16 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/*  program to convert Kilometer into meter and display the result. */
 public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        /* Input the distance */
         System.out.println("Enter the distance in Kilometer");
         int n = s.nextInt();
         System.out.println("Distance in Kilometer: "+n+"km");
+        /* Converting Kilometer to meter*/
         System.out.println("Distance in meter: "+(n*1000)+"m");
 
     }
