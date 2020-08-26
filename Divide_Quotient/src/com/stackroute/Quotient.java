573afb964440eb87df05110dfdaafebeb8767069
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 21:17:14 +0530

    Updated comments.

--- Divide_Quotient/src/com/stackroute/Quotient.java
+++ Divide_Quotient/src/com/stackroute/Quotient.java
@@ -1,17 +1,18 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/* Program to input 2 numbers, divide the first number by second number and display their quotient.*/
 public class Quotient {
 
     public static void main(String[] args) {
+        /* Input Variables */
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the 1st number:");
         int a = s.nextInt();
         System.out.println("Enter the 2nd number:");
         int b = s.nextInt();
-        if(a<b)
-        {
+        /* Check if the 2nd number is greater than 1 so division not possible */
+        if(a<b){
             System.out.println("The 2nd number is greater than 1st, so Quotient is 0");
         }
         else {
