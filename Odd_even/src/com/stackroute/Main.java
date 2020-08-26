2fb6552441731765a7b34ee9543a5eff96694d05
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 21:25:07 +0530

    Update comment

--- Odd_even/src/com/stackroute/Main.java
+++ Odd_even/src/com/stackroute/Main.java
@@ -1,19 +1,19 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/* Program to check if the entered value is even or Odd*/
 public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+/* Input the number*/        
         System.out.println("Enter the number:");
         int n = s.nextInt();
-        if(n%2==0)
-        {
+/* Checki the number is odd or even and print the result*/
+        if(n%2==0){
             System.out.println("The entered value is Even");
         }
-        else
-        {
+        else{
             System.out.println("The entered value is Odd");
         }
     }
